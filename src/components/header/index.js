afef67e2bd6079fd373aa30b194261bca08bfe4f
Abraham <cedenoabraham@gmail.com>
2019-11-24 23:37:53 -0300

    update site content

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <header className="header">
             <div>
-                <h1>Hi, I'm Abraham.</h1>
+                <h1>{'<Hi />'}, I'm Abraham.</h1>
                 <span className="mail">
                     <a href="mailto:cedenoabraham@gmail.com">cedenoabraham(at)gmail.com</a>
                 </span>
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,11 +7,18 @@ export default () => (
             <h3>Software Developer.</h3>
             <p>
                 I'm a passionate frontend developer who loves building software and contributing 
-                to open source projects, keeps up to date with new trends and tries to level up 
-                coding skills.
+                to open source projects, tries to keep up to date with new trends and is eager to 
+                level up coding skills.
             </p>
             <br />
-            <p>Check out some of my side projects and overall resume in the links below.</p>
+            <p>
+                High understanding of software architecture design and awareness of how to create 
+                software which is scalable, easily maintainable, extensible and easy to test.
+            </p>
+            <br />
+            <p>
+                Check out some of my side projects and overall resume in the links below.
+            </p>
         </article>
     </Layout>
 );
