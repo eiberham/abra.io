cc8f812f1c2b43640217d3979afba8190b9f655d
Abraham <cedenoabraham@gmail.com>
2020-06-27 08:47:49 -0300

    dark mode

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import './styles.scss';
 
+import Toggle from 'react-toggle';
+import { IoIosSunny, IoIosMoon } from "react-icons/io";
+
+import { ThemeToggler } from 'gatsby-plugin-dark-mode';
+
+
+import 'react-toggle/style.css';
+
 const Header = () => {
+    /* const [dark, setDark] = useState(false);
+    const onToggleChange = useCallback(()=> {
+        toggleTheme(dark ? 'dark' : 'light')
+    }, [dark]) */
     return (
         <header className="header">
             <div>
                 <h1>{'<Hi />'}, I'm Abraham.</h1>
+                <ThemeToggler>
+                    {({theme, toggleTheme}) => (
+                        <Toggle
+                            defaultChecked={theme === 'dark'}
+                            className='dark-mode'
+                            icons={{
+                            unchecked: <IoIosSunny color={'yellow'} size={20} />,
+                            checked: <IoIosMoon color={'yellow'} size={20} />,
+                            }}
+                            onChange={ e => toggleTheme(e.target.checked ? 'dark' : 'light')} 
+                        />
+                    )}
+                </ThemeToggler>
                 <span className="mail">
                     <a arial-label="email" href="mailto:cedenoabraham@gmail.com">cedenoabraham(at)gmail.com</a>
                 </span>
