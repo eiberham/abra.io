89fc7c0fa10744d27d7c9cb7475558a93ddf1c8b
Abraham <cedenoabraham@gmail.com>
2020-06-27 02:00:01 -0300

    got rid of react lazy

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,19 +1,17 @@
-import React, { lazy } from 'react';
+import React from 'react';
 import './styles.scss';
 import { Helmet } from "react-helmet"
 import { css, Global } from "@emotion/core"
-
-const Header = lazy(() => import('../header'));
-const Footer = lazy(() => import('../footer'));
+import Header from '../header';
+import Footer from '../footer';
 
 const Layout = ({children}) => {
     return (
         <React.Fragment>
             <Helmet>
                 <title>Abraham</title>
-                <link rel="apple-touch-icon" href="/example.png"></link>
-                <link rel="preload" as="style" href="https://fonts.googleapis.com/css?family=Roboto&display=swap" onload="this.onload=null;this.rel='stylesheet'"/>
-                <link rel="preload" as="style" href="https://fonts.googleapis.com/css?family=Odibee+Sans&display=swap" onload="this.onload=null;this.rel='stylesheet'"></link>
+                <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css?family=Odibee+Sans&display=swap" rel="stylesheet"></link>
                 <meta name="description" content="Abraham's Portfolio"></meta>
                 <meta name="keywords" content="Software, Developer, Frontend, Portfolio"></meta>
             </Helmet>
