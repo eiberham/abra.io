658bec47bbb5fe4fa6b556ae7c1deead02f8f5ad
Abraham <cedenoabraham@gmail.com>
2025-08-04 15:20:06 -0300

    fix footer year

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -5,12 +5,13 @@ import Newsletter from '../newsletter'
 import './styles.scss';
 
 const Footer = () => {
+    const year = new Date().getFullYear()
     return (
         <footer className="footer">
             <section>
                 <Newsletter />
                 <div className="footer__bottom">
-                    <span>Content on this Site is Copyright © 2022 Abraham. Licensed under a <a aria-label="creative-commons" href="https://creativecommons.org/licenses/by/3.0">Creative Commons Attribution 3.0 License</a></span>
+                    <span>Content on this Site is Copyright © {year} Abraham. Licensed under a <a aria-label="creative-commons" href="https://creativecommons.org/licenses/by/3.0">Creative Commons Attribution 3.0 License</a></span>
                     <div className="footer__follow">
                         <a aria-label="github" href="https://github.com/eiberham"><FaGithubSquare /></a>
                     </div>
